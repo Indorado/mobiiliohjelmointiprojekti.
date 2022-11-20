dccb68d98c0798fda361c1a1c73bb91142478a72
Tiia Riikonen <tiiariikonen@outlook.com>
2022-11-20 15:30:45 +0200

    Sijainnin pyytäminen lisätty. Myös ilmanpaine haettu OpenWeather API:sta, samoin FirstFragment sivulle on lisätty nappi yläkulmaan mahdollisille asetuksille (ei siis tee vielä yhtään mitään...)

--- app/src/main/java/com/example/mobiiliohjelmointiryhmaq/FirstFragment.java
+++ app/src/main/java/com/example/mobiiliohjelmointiryhmaq/FirstFragment.java
@@ -1,4 +1,5 @@
 package com.example.mobiiliohjelmointiryhmaq;
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,6 +11,8 @@ import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -31,7 +34,7 @@ public class FirstFragment extends AppCompatActivity {
 
     private RelativeLayout Koti;
     private ProgressBar LadataanSivu;
-    private TextView KaupunginNimi, Lampotila, Ehto, pvm, tuntuuKuin, tuulenNps, ilmanKst, nykyinenMaa;
+    private TextView KaupunginNimi, Lampotila, Ehto, pvm, tuntuuKuin, tuulenNps, ilmanKst, nykyinenMaa, ilmanpaine;
     private TextInputEditText MuokkaaKaupunkia;
     private ImageView TaustaVariIV, SaaTilaIV, EtsiLogoIV;
     private RecyclerView RecycleSaaEnnuste;
@@ -41,7 +44,6 @@ public class FirstFragment extends AppCompatActivity {
     String str = formatter.format(date);
 
     private final String url = "https://api.openweathermap.org/data/2.5/weather?q=Kuopio&units=metric&appid=a012a806d418509506a86dcde2dc62bb";
-    // Tästä tehdään GPS homma jos ehtii..
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -58,6 +60,7 @@ public class FirstFragment extends AppCompatActivity {
         tuulenNps = findViewById(R.id.TuulenVoimakkuusTeksti);
         ilmanKst = findViewById(R.id.IlmankosteusTeksti);
         nykyinenMaa = findViewById(R.id.idMaa);
+        ilmanpaine = findViewById(R.id.IlmanPaineTeksti);
 
         pvm = findViewById(R.id.idDate);
         button = (Button) findViewById(R.id.idHaeLisaaBtn);
@@ -70,15 +73,19 @@ public class FirstFragment extends AppCompatActivity {
         //RecycleSaaEnnuste = findViewById(R.id.idRecycleSaaEnnuste);
         //EtsiLogoIV = findViewById(R.id.idEtsiLogo);
 
+        // Datan näyttäminen
         getData();
+
         // Toinen sivu
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                SecondFragment();
+                goToSecondFragment();
             }
         });
 
+        // Sijaintitiedot pyydetään vain ensimmäisen kerran sovelluksen käynnistämisen yhteydessä
+        accessLocation();
     }
 
     @SuppressLint("SetTextI18n")
@@ -101,14 +108,16 @@ public class FirstFragment extends AppCompatActivity {
                 String wind = jsonWind.getString("speed");// TUULENNOPEUS
                 String humidity = jsonMain.getString("humidity");// KOSTEUS
                 String feelsLike = jsonMain.getString("feels_like"); // TUNTUU KUIN
+                String pressure = jsonMain.getString("pressure");
                 String maa = jsonObjectSys.getString("country");// MAA
-                // String cityName = jsonResponse.getString("name");// KAUPUNKI
+                // String cityName = jsonResponse.getString("name");// KAUPUNKI --> Jos haetaan tieto jostain muualta kuin String
 
                 // Määritetään tekstit
                 Lampotila.setText(currentWeather + " °C");
                 tuntuuKuin.setText(feelsLike + " °C");
                 tuulenNps.setText(wind + " m/s");
-                ilmanKst.setText(humidity + "g/m³");
+                ilmanKst.setText(humidity + " g/m³");
+                ilmanpaine.setText(pressure + " Pa");
                 nykyinenMaa.setText(maa);
 
                 pvm.setText(str);
@@ -124,8 +133,38 @@ public class FirstFragment extends AppCompatActivity {
         KaupunginNimi.setText("Kuopio,");//Voidaan mahdollisesti hakea GPS avulla, jos jää aikaa.
     }
     // Lähdetään toiselle sivulle
-    public void SecondFragment() {
+    public void goToSecondFragment() {
         Intent intent = new Intent(this, SecondFragment.class);
         startActivity(intent);
+    }
+
+    // Sijaintitiedot
+    public void accessLocation() {
+        ActivityResultLauncher<String[]> locationPermissionRequest =
+                registerForActivityResult(new ActivityResultContracts
+                        .RequestMultiplePermissions(), result -> {
+                    Boolean fineLocationGranted = null;
+                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+                        fineLocationGranted = result.getOrDefault(
+                                Manifest.permission.ACCESS_FINE_LOCATION, false);
+                    }
+                    Boolean coarseLocationGranted = null;
+                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+                        coarseLocationGranted = result.getOrDefault(
+                                Manifest.permission.ACCESS_COARSE_LOCATION,false);
+                    }
+                    if (fineLocationGranted != null && fineLocationGranted) {
+                        // Precise location access granted.
+                    } else if (coarseLocationGranted != null && coarseLocationGranted) {
+                        // Only approximate location access granted.
+                    } else {
+                        // No location access granted.
+                    }
+                });
+
+        locationPermissionRequest.launch(new String[] {
+                android.Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+        });
     }
 }
